6ae1a96c2291bec7cff73d37ba89d42e30da1f5d
Spenalva1 <penalvasantiagogm@gmail.com>
2020-05-30 14:39:38 -0300

    estado_nivel y estado_juego implementadas

--- defendiendo_torres.c
+++ defendiendo_torres.c
@@ -1,4 +1,6 @@
 #include "defendiendo_torres.h"
+#include <stdbool.h>
+
 #define VIDA_INICIAL_TORRE 600
 #define ENANOS_EXTRA 10
 #define ELFOS_EXTRA 10
@@ -14,6 +16,14 @@
 #define CRITICO_LEGOLAS_MALO 0
 #define CRITICO_LEGOLAS_REGULAR 10
 #define CRITICO_LEGOLAS_BUENO 25
+#define NIVEL_ULTIMO 4
+#define NIVEL_TERMINADO 1
+#define NIVEL_JUGANDO 0
+#define JUEGO_GANADO 1
+#define JUEGO_PERDIDO -1
+#define JUEGO_JUGANDO 0
+#define TORRES_CON_VIDA 1
+#define TORRE_DESTRUIDA 0
 
 void inicializar_juego(juego_t* juego, int viento, int humedad, char animo_legolas, char animo_gimli){
 	juego->torres.resistencia_torre_1 = VIDA_INICIAL_TORRE;
@@ -36,4 +46,35 @@ void inicializar_juego(juego_t* juego, int viento, int humedad, char animo_legol
 		juego->critico_legolas = CRITICO_LEGOLAS_REGULAR;
 	else
 		juego->critico_legolas = CRITICO_LEGOLAS_MALO;
+
+	juego->nivel_actual = 1;
 }
+
+int estado_nivel(nivel_t nivel){
+	int nivel_estado = NIVEL_TERMINADO;
+	int i = 0;
+	while((nivel_estado == NIVEL_TERMINADO) && (i < nivel.tope_enemigos)){
+		if(nivel.enemigos[i].vida > 0)
+			nivel_estado = NIVEL_JUGANDO;
+		i++;
+	}
+	return nivel_estado;
+}
+
+/*
+*
+*/
+int torres_con_vida(torres_t torres){
+	if ((torres.resistencia_torre_1 > 0) && (torres.resistencia_torre_2 > 0))
+		return TORRES_CON_VIDA;
+	return TORRE_DESTRUIDA;
+}
+
+int estado_juego(juego_t juego){
+	if(torres_con_vida(juego.torres) == TORRE_DESTRUIDA)
+		return JUEGO_PERDIDO;
+	if((juego.nivel_actual == NIVEL_ULTIMO) && (estado_nivel(juego.nivel) == 1))
+		return JUEGO_GANADO;
+	return JUEGO_JUGANDO;
+}
+
--- juego.c
+++ juego.c
@@ -8,7 +8,5 @@ int main(){
     animos(&viento, &humedad , &animo_legolas , &animo_gimli);
     juego_t juego;
     inicializar_juego(&juego, viento, humedad, animo_legolas, animo_gimli);
-    printf("resistencia 1: %d. resistencia 2: %d. enanos: %d. elfos: %d.\n", juego.torres.resistencia_torre_1, juego.torres.resistencia_torre_2, juego.torres.enanos_extra, juego.torres.elfos_extra);
-    printf("critico_l: %d. critico_g: %d. fallo_l: %d. fallo_g: %d\n", juego.critico_legolas, juego.critico_gimli, juego.fallo_legolas, juego.fallo_gimli);
     return 0;
 }
